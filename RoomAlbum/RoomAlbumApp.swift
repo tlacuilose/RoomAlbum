9eac12e65a875304c4868a5d137dc81a16e42e99
Jose Tlacuilo <tlacuilose@outlook.com>
2021-06-28 22:27:18 -0600

    Creation

---
+++ RoomAlbum/RoomAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomAlbumApp.swift
+//  RoomAlbum
+//
+//  Created by Jose Tlacuilo on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct RoomAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
