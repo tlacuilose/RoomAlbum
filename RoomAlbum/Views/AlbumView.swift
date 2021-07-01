494ae1020ae8b680400ae95563ffe14ae680a39d
Jose Tlacuilo <tlacuilose@outlook.com>
2021-06-30 23:36:15 -0600

    Adds inserting new content views.

--- RoomAlbum/Views/AlbumView.swift
+++ RoomAlbum/Views/AlbumView.swift
@@ -10,11 +10,41 @@ import SwiftUI
 struct AlbumView: View {
     let album: Album
     
+    @State(initialValue: false) var presentNewPhoto: Bool
+    
     var body: some View {
         List {
             ForEach(album.photos) { photo in
                 Text(photo.url)
             }
         }
+        .navigationTitle(album.name)
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Add") {
+                    self.presentNewPhoto = true
+                }
+            }
+        }
+        .sheet(isPresented: $presentNewPhoto) {
+            NavigationView {
+                Image(systemName: "photo")
+                    .navigationTitle("New Photo")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                self.presentNewPhoto = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                self.presentNewPhoto = false
+                            }
+                        }
+                    }
+            }
+        }
+        
     }
 }
--- RoomAlbum/Views/RoomView.swift
+++ RoomAlbum/Views/RoomView.swift
@@ -10,11 +10,43 @@ import SwiftUI
 struct RoomView: View {
     let room = Room()
     
+    @State(initialValue: false) var presentNewAlbum: Bool
+    @State(initialValue: "") var newAlbumName: String
+    
     var body: some View {
         NavigationView {
             AlbumList(albums: room.albums)
-                .navigationTitle("Albums")
+            .navigationTitle("Albums")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add") {
+                        self.presentNewAlbum = true
+                    }
+                }
+            }
         }
+        .sheet(isPresented: $presentNewAlbum) {
+            NavigationView {
+                Form {
+                    TextField("Name", text: $newAlbumName)
+                }
+                .navigationTitle("New Album")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Cancel") {
+                            self.presentNewAlbum = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Done") {
+                            self.presentNewAlbum = false
+                        }
+                    }
+                }
+            }
+        }
+       
     }
 }
 
